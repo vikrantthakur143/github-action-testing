2a3dbe590c3ebea36082530ac494581b4b8f14fc
Vikrant Thakur <41588914+vikrantthakur143@users.noreply.github.com>
2026-07-03 03:00:39 +0530

    Create python-code.py

---
+++ python-code.py
@@ -0,0 +1,11 @@
+from flask inport Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return render_template('index.html')
+
+
+@app.route('/health')
+def health():
+  return 'Server is up and running'
